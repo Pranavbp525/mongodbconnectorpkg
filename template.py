091a22f794e67b8f78b34f842ad0bb4c62058ee7
Pranavbp525 <53532240+Pranavbp525@users.noreply.github.com>
2024-06-20 20:22:21 -0400

    latest commit

--- template.py
+++ template.py
@@ -23,7 +23,7 @@ list_of_files = [
    "experiments/experiments.ipynb", 
 ]
 
-print("running")
+
 for filepath in list_of_files:
     filepath = Path(filepath)
     filedir, filename = os.path.split(filepath)
